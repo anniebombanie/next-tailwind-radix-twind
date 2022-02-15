f97b7658ae11f438966d9642050a48bf88892bb8
anniebombanie <anniebombanie@gmail.com>
2022-02-15 17:14:14 -0500

    login page looking decent

---
+++ pages/login.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+import Link from 'next/link'
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props)
+    this.mustBeLoggedOut = true
+  }
+
+  handleSubmit = ({ email, password }) => {
+    console.log({ email, password })
+  }
+
+  render() {
+    return (
+      <html class="min-h-screen bg-slate-100">
+        <div className="login-form">
+          <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                className="mx-auto h-14 w-auto"
+                src="icon.svg"
+                alt="Workflow"
+              />
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Log in to Pastel
+              </h2>
+              <p className="mt-2 text-center text-sm text-gray-600">
+                Or{' '}
+                <a
+                  href="#"
+                  className="font-medium text-blue-500 hover:text-blue-600"
+                >
+                  start your 14-day free trial
+                </a>
+              </p>
+            </div>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+              <div className="bg-white py-10 px-6 sm:rounded-lg sm:px-10">
+                <form className="space-y-6" action="#" method="POST">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email address
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className='flex justify-center'>
+                    <button
+                      type="submit"
+                      className="flex w-36 align-center justify-center rounded-md border border-transparent transition-colors bg-blue-500 py-3 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    >
+                      Log in
+                    </button>
+                  </div>
+                </form>
+
+                <div className="mt-6">
+                  <div className="mt-6 text-center">
+                    <a
+                      href="#"
+                      className="block text-sm font-medium text-blue-500 hover:text-blue-600"
+                    >
+                      Forgot your password?
+                    </a>
+
+                    <a
+                      href="#"
+                      className="mt-3 block text-sm font-medium text-blue-500 hover:text-blue-600"
+                    >
+                      Don't have an account?
+                    </a>
+
+                    <a
+                      href="#"
+                      className="mt-3 block text-sm font-medium text-blue-500 hover:text-blue-600"
+                    >
+                      Log in with SSO
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </html>
+    )
+  }
+}
+
+export default LoginPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        black: '#000000',
+        blue: {
+          50: '#e2e9ff',
+          500: '#4f77ff',
+          600: '#2953e2',
+        },
+        slate: {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
